47e739d9430ce10495d6445fc700589dece3028d
HotariTobu <hotari24tools@gmail.com>
2023-11-04 14:25:11 +0900

    add: timeit

---
+++ timeit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace timeit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            if (args.Length < 1)
+            {
+                return;
+            }
+
+            using (Process process = new())
+            {
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = args[0],
+                    Arguments = string.Join(" ", args.Skip(1).ToArray()),
+                };
+
+                Stopwatch stopwatch = new();
+                stopwatch.Start();
+                process.Start();
+                process.WaitForExit();
+                stopwatch.Stop();
+
+                Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+            }
+        }
+    }
+}
